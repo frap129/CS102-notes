b30c248e8af9dc7e7c6b8fdb21ecaf7494177a50
Joe Maples <joe@frap129.org>
2018-04-06 14:20:23 -0400

    Add TowersOfHanoi
    Signed-off-by: Joe Maples <joe@frap129.org>

---
+++ src/cs102/notes/TowersOfHanoi.java
@@ -0,0 +1,20 @@
+package cs102.notes;
+
+public class TowersOfHanoi {
+    public static void main(String args[])
+    {
+        int n = 4; // Number of disks
+        solveTowersOfHanoi(n, 'A', 'C', 'B');
+    }
+    private static void solveTowersOfHanoi(int n, char from_rod, char to_rod, char aux_rod)
+    {
+        if (n == 1)
+        {
+            System.out.println("Move disk 1 from rod " +  from_rod + " to rod " + to_rod);
+            return;
+        }
+        solveTowersOfHanoi(n-1, from_rod, aux_rod, to_rod);
+        System.out.println("Move disk " + n + " from rod " +  from_rod + " to rod " + to_rod);
+        solveTowersOfHanoi(n-1, aux_rod, to_rod, from_rod);
+    }
+}
